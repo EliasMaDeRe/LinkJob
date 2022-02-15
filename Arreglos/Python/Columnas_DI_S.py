e09fef4638b08f97a173b9bb5ff8206187981e5f
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2022-02-14 20:33:00 -0600

    Add files via upload

---
+++ Arreglos/Python/Columnas_DI_S.py
@@ -0,0 +1,21 @@
+'''
+Author: "Space Team"
+Date: 12/02/2022
+Description: Printing the upper diagonal of a matrix traversed by rows
+'''
+
+#Input
+range = [0,1,2,3,4]
+Dimension = 5
+filas = 0
+columnas = 0
+
+#Algorithm
+for columnas in range:
+    Dimension = Dimension-1  
+    for filas in range:
+        if filas <= Dimension:
+            print(f"({filas},{columnas}) ", end= "")      
+    print(" ")   
+
+#Output        
