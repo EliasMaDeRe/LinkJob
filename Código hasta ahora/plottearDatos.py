55909abfb0f61eafce9d568d6fd00dd2c12b97a6
Carlos May <97546829+CarlosMay7@users.noreply.github.com>
2022-05-22 04:19:06 -0500

    Update plottearDatos.py

--- Código hasta ahora/plottearDatos.py
+++ Código hasta ahora/plottearDatos.py
@@ -7,6 +7,7 @@ Created on Wed May 18 11:39:19 2022
 import pandas as pd
 import matplotlib.pyplot as plt
 import plottearTecnologias as pt
+import math as mt
 
 #Plottear salario
 
@@ -63,14 +64,16 @@ def plottear_locacion(df):
     plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
     plt.close()
     
-    '''
+    
 def plottear_tecnologias(df):
     df = pd.read_csv('LT.csv')
     df[['Tecnologia']].plot(kind='bar',color="darkorange,",title = "Frecuencia de aparicion de las tecnologias en las ofertas de trabajo")
     plt.savefig('tecnologias.png',dpi=300,bbox_inches='tight')
     plt.close()
-    '''
     
+    
+#Estas las hace Joachin
+'''
 def plottear_modalidad_EG(df):
     
     
@@ -98,6 +101,24 @@ def plottear_modalidad_EM(df):
     plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
     plt.close()
     
+def plottear_modalidad_EP(df):
+    
+    
+    dfDatos = pd.DataFrame(df['Presencial Grande'])
+    dfDatos.plot(kind= 'bar', x='Presencial Grande', y= 0)
+    #dfDatos2 = dfDatos.groupby('Tamaño Empresa')['Tamaño Empresa'].count()
+    #tplot = dfDatos.plot(xlabel ="Empresas grandes con modalidad presencial",ylabel="Cantidad de ofertas",color = "darkorange", kind='bar', title = "Cantidad de ofertas por modalidad presencial", width =.2,legend=False)
+    #tplot.annotate('Total de ofertas: '+str(dfDatos['Tamaño Empresa'].count()), xy=(dfDatos2.count()*.5,dfDatos2.max()*.8), ha='center', va='bottom')
+    for p in dfDatos.patches:
+        dfDatos.annotate(str(p.get_height()),xy=((p.get_x()),p.get_height()),ha='left',va='bottom')
+        
+    plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
+    plt.close()
+    
+'''
+    
+    #Estas las debe estar haciendo vale
+'''
 def plottear_modalidad_EG(df):
     
     
@@ -110,7 +131,8 @@ def plottear_modalidad_EG(df):
         
     plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
     plt.close()
-    
+   
+
 def plottear_locacion_EM(df):
     dfDatos = pd.DataFrame(df['Locacion', 'Modalidad'])
     dfDatos2 = dfDatos.groupby('Locación')['Modalidad'].count()
@@ -158,7 +180,9 @@ def plottear_locacion_Vir(df):
         
     plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
     plt.close()
-    
+'''
+#Estas las debe estar haciendo Leo    
+'''
 def plottear_TE_vir(df):
     
     
@@ -172,6 +196,7 @@ def plottear_TE_vir(df):
     plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
     plt.close()
 
+
 def plottear_TE_pres(df):
     
     
@@ -184,38 +209,218 @@ def plottear_TE_pres(df):
         
     plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
     plt.close()
-
+'''
  
     
+#Estas son de nosotros 
     
-    '''
 def plottear_salario_locacion(df):
     
     dfDatos = df
     dfDatos2 = dfDatos.groupby('Locacion')['Salario Mensual'].sum()
+    dfDatos2.columns= ['Locacion','Suma']
     dfDatos3 = dfDatos.groupby('Locacion')['Salario Mensual'].count()
+    dfDatos2.columns= ['Locacion','Cantidd']
+
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
     
-    dfPromedio=pd.Series()
+    df4['Promedio'] = df4['Suma']/ df4['Cantidad']
+    dfsalario_locacion = pd.DataFrame(df4['Promedio'])
+    tplot = dfsalario_locacion.plot(xlabel ="Locación",ylabel="Salario en pesos",color = "darkorange", kind='bar', title = "Salario promedio por locación en México", width =.2,legend=False)
+    tplot.annotate('Total de ofertas: '+str(dfDatos3.sum()), xy=(dfDatos3.count()*.75,dfDatos3.max()*0), ha='center', va='bottom')
+    for p in tplot.patches:
+        tplot.annotate(str(p.get_height()),xy=((p.get_x()),p.get_height()),ha='left',va='bottom')
+    
+    '''
+    plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
+    plt.close()
     
     
-    for i in range dfDatos2.shape[0] :
+    df4.to_csv('df4.csv', index=False)
+    '''
+    print(dfDatos2)
+    print(dfDatos3)
+    print(df4)
+    
+    
+def plottear_salario_eg (df):
+    
+    dfDatos = df
+    dfDatos2 = dfDatos.groupby('Tamaño Empresa')['Salario Mensual'].sum()
+    dfDatos2.columns= ['Tamaño Empresa','Suma']
+    dfDatos3 = dfDatos.groupby('Tamaño Empresa')['Salario Mensual'].count()
+    dfDatos2.columns= ['Tamaño Empresa','Cantidd']
+    
+
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4['Promedio'] = df4['Suma']/ df4['Cantidad']
+    dfsalario_grande_ambos = pd.DataFrame(df4['Promedio'])
+    dfsalario_grande= dfsalario_grande_ambos.drop(['Pequeña'])
+    tplot = dfsalario_grande.plot(xlabel ="Tamaño",ylabel="Salario en pesos",color = "darkorange", kind='bar', title = "Salario promedio por empresas grandes en México", width =.2,legend=False)
+    tplot.annotate('Total de ofertas: '+str(df4['Cantidad']), xy=(dfDatos3.count()*.75,dfDatos3.max()*0), ha='center', va='bottom')
+    for p in tplot.patches:
+        tplot.annotate(str(p.get_height()),xy=((p.get_x()),p.get_height()),ha='left',va='bottom')
+    
+    '''
+    plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
+    plt.close()
+    
+    
+    df4.to_csv('df4.csv', index=False)
+    
+    '''
+    print(dfDatos2)
+    print(dfDatos3)
+    print(df4)
+    
+    
+def plottear_salario_em (df):
+    
+    dfDatos = df
+    dfDatos2 = dfDatos.groupby('Tamaño Empresa')['Salario Mensual'].sum()
+    dfDatos2.columns= ['Tamaño Empresa','Suma']
+    dfDatos3 = dfDatos.groupby('Tamaño Empresa')['Salario Mensual'].count()
+    dfDatos2.columns= ['Tamaño Empresa','Cantidd']
+    
+
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4['Promedio'] = df4['Suma']/ df4['Cantidad']
+    dfsalario_grande_ambos = pd.DataFrame(df4['Promedio'])
+    dfsalario_grande= dfsalario_grande_ambos.drop(['Mediana'])
+    tplot = dfsalario_grande.plot(xlabel ="Tamaño",ylabel="Salario en pesos",color = "darkorange", kind='bar', title = "Salario promedio por empresas medianas en México", width =.2,legend=False)
+    tplot.annotate('Total de ofertas: '+str(df4['Cantidad']), xy=(dfDatos3.count()*.75,dfDatos3.max()*0), ha='center', va='bottom')
+    for p in tplot.patches:
+        tplot.annotate(str(p.get_height()),xy=((p.get_x()),p.get_height()),ha='left',va='bottom')
         
-        a = dfDatos2[0].loc[i] / dfDatos3[0].loc[i]
-        dfPromedio[0].loc[i] = a
-        print(a)
-            
-            #dpPromedio = [dfDatos2[0].loc[i]/dfDatos3[0].loc[i]]
-        
-        
+    plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
+    plt.close()
+    
     
     print(dfDatos2)
     print(dfDatos3)
-    '''
+    print(df4)
     
-    #tplot = dfDatos.groupby('Salario Mensual')['Salario Mensual'].count().plot(xlabel ="Salario mensual estimado",ylabel="Cantidad de ofertas",color = "darkorange", kind='bar', title = "Cantidad de ofertas por salario mensual estimado",legend=False)
-    #tplot.annotate('Total de ofertas: '+str(dfDatos2.sum()), xy=(dfDatos2.count()*.7,dfDatos2.max()*.8), ha='center', va='bottom')
-    #for p in tplot.patches:
-        #tplot.annotate(str(p.get_height()),xy=((p.get_x()),p.get_height()),ha='left',va='bottom')
-        
-    #plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
+def plottear_salario_ep (df):
+    
+    dfDatos = df
+    dfDatos2 = dfDatos.groupby('Tamaño Empresa')['Salario Mensual'].sum()
+    dfDatos2.columns= ['Tamaño Empresa','Suma']
+    dfDatos3 = dfDatos.groupby('Tamaño Empresa')['Salario Mensual'].count()
+    dfDatos2.columns= ['Tamaño Empresa','Cantidd']
+    
+
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4['Promedio'] = df4['Suma']/ df4['Cantidad']
+    dfsalario_grande_ambos = pd.DataFrame(df4['Promedio'])
+    dfsalario_grande= dfsalario_grande_ambos.drop(['Grande'])
+    tplot = dfsalario_grande.plot(xlabel ="Tamaño",ylabel="Salario en pesos",color = "darkorange", kind='bar', title = "Salario promedio por empresas pequeñas en México", width =.2,legend=False)
+    tplot.annotate('Total de ofertas: '+str(df4['Cantidad']), xy=(dfDatos3.count()*.75,dfDatos3.max()*0), ha='center', va='bottom')
+    for p in tplot.patches:
+        tplot.annotate(str(p.get_height()),xy=((p.get_x()),p.get_height()),ha='left',va='bottom')
+    '''
+    plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
+    plt.close()
+    
+    
+    df4.to_csv('df4.csv', index=False)
+    
+    '''
+    print(dfDatos2)
+    print(dfDatos3)
+    print(df4)
+    
+def plottear_salario_presencial(df):
+    
+    dfDatos = df
+    dfDatos2 = dfDatos.groupby('Modalidad')['Salario Mensual'].sum()
+    dfDatos2.columns= ['Modalidad','Suma']
+    dfDatos3 = dfDatos.groupby('Modalidad')['Salario Mensual'].count()
+    dfDatos2.columns= ['Modalidad','Cantidd']
+    
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4['Promedio'] = df4['Suma']/ df4['Cantidad']
+    dfsalario_grande_ambos = pd.DataFrame(df4['Promedio'])
+    dfsalario_grande= dfsalario_grande_ambos.drop(['Virtual'])
+    tplot = dfsalario_grande.plot(xlabel ="Modalidad",ylabel="Salario en pesos",color = "darkorange", kind='bar', title = "Salario promedio por modalidad presencial en México", width =.2,legend=False)
+    tplot.annotate('Total de ofertas: '+str(df4['Cantidad']), xy=(dfDatos3.count()*.75,dfDatos3.max()*0), ha='center', va='bottom')
+    for p in tplot.patches:
+        tplot.annotate(str(p.get_height()),xy=((p.get_x()),p.get_height()),ha='left',va='bottom')
+    '''
+    plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
+    plt.close()
+    
+    
+    df4.to_csv('df4.csv', index=False)
+    
+    '''
+    print(dfDatos2)
+    print(dfDatos3)
+    print(df4)
+    
+def plottear_salario_virtual(df):
+    
+    dfDatos = df
+    dfDatos2 = dfDatos.groupby('Modalidad')['Salario Mensual'].sum()
+    dfDatos2.columns= ['Modalidad','Suma']
+    dfDatos3 = dfDatos.groupby('Modalidad')['Salario Mensual'].count()
+    dfDatos2.columns= ['Modalidad','Cantidd']
+    
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4 = pd.DataFrame()
+    df4= pd.concat([dfDatos2, dfDatos3], axis=1)
+    df4.columns= ['Suma', 'Cantidad']
+    
+    df4['Promedio'] = df4['Suma']/ df4['Cantidad']
+    dfsalario_grande_ambos = pd.DataFrame(df4['Promedio'])
+    dfsalario_grande= dfsalario_grande_ambos.drop(['Presencial'])
+    tplot = dfsalario_grande.plot(xlabel ="Modalidad",ylabel="Salario en pesos",color = "darkorange", kind='bar', title = "Salario promedio por modalidad virtual en México", width =.2,legend=False)
+    tplot.annotate('Total de ofertas: '+str(df4['Cantidad']), xy=(dfDatos3.count()*.75,dfDatos3.max()*0), ha='center', va='bottom')
+    for p in tplot.patches:
+        tplot.annotate(str(p.get_height()),xy=((p.get_x()),p.get_height()),ha='left',va='bottom')
+    '''
+    plt.savefig('testplot.png',dpi=300,bbox_inches='tight')
+    plt.close()
+    
+    
+    df4.to_csv('df4.csv', index=False)
+    
+    '''
+    print(dfDatos2)
+    print(dfDatos3)
+    print(df4)
+    
+    
     
